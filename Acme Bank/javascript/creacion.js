6c1a62c6d1517dfdda2ef1d54cc3f127bb73f41a
Samuel Torres <torressanchezsamuelfelipe@gmail.com>
2025-04-10 20:59:15 -0500

    Add files via upload

---
+++ Acme Bank/javascript/creacion.js
@@ -0,0 +1,9 @@
+function mostrarAcciones() {
+    document.getElementById("creacion").style.display = "none";
+    document.getElementById("acciones").style.display = "block";
+}
+
+function volverFormulario() {
+    document.getElementById("acciones").style.display = "none";
+    document.getElementById("creacion").style.display = "block";
+}
